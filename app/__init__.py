db10883bb2a19e19153bd2f68bb10b1d30570e19
Krusty <jspell@gmail.com>
2015-12-07 14:37:12 -0600

    Added email functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask, g
 from flask.ext.login import LoginManager
+from flask.ext.mail import Mail
+
 app = Flask(__name__)
 app.config.from_object('config')
 # from user import User
 
+mail = Mail(app)
 
 login_manager = LoginManager()
 login_manager.login_view

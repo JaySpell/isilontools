08260b498841bcb00d39e3d65bf0f7bd50fb5f88
Krusty <jspell@gmail.com>
2016-05-31 14:41:28 -0500

    yet again

--- app/views.py
+++ app/views.py
@@ -163,8 +163,8 @@ def cost():
         return render_template('finish.html', name=name,
             new_limit=new_thresh_GB, email_status=email_status)
 
-    flash("Correct errors and resubmit..."")
-    
+    flash("Correct errors and resubmit...")
+
     return render_template('cost.html', form=myform,
         btn_txt="Add Space", title="Enter Customer Name & Cost Center")
 
